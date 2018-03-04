2c3326b56488cbd637a695d2f9485bc91f813fd4
zephinzer <dev@joeir.net>
2018-03-04 14:02:50 +0800

    fixed code climate verbosity issue

--- src/server/readiness/controller.js
+++ src/server/readiness/controller.js
@@ -12,6 +12,58 @@ module.exports = {
     cache: null,
   },
   getStatus,
+  getRedisStatus,
+  getMySqlStatus,
+};
+
+/**
+ * Gets the connection status of the Redis cache
+ *
+ * @return {Promise}
+ */
+function getRedisStatus() {
+  return new Promise((resolve, reject) => {
+    const connectionTest = redis.createClient({
+      host: config.cache.host,
+      port: config.cache.port,
+    });
+    connectionTest.on('error', (err) => {
+      module.exports.status.cache = false;
+      module.exports.error.cache = err;
+      connectionTest.quit();
+      connectionTest.unref();
+      reject(err);
+    }).on('connect', () => {
+      module.exports.status.cache = true;
+      module.exports.error.cache = false;
+      connectionTest.quit();
+      connectionTest.unref();
+      resolve();
+    });
+  });
+};
+
+/**
+ * Resolves the status of the MySQL database
+ *
+ * @return {Promise}
+ */
+function getMySqlStatus() {
+  return new Promise((resolve, reject) => {
+    const connectionTest = mysql.createConnection({
+      host: config.database.host,
+      port: config.database.port,
+      user: config.database.auth.username,
+      password: config.database.auth.password,
+      database: config.database.name,
+    });
+    connectionTest.execute('SELECT 1+1;', (err, res, fields) => {
+      module.exports.status.database = (err == null);
+      module.exports.error.database = err ? err : false;
+      connectionTest.close();
+      (err) && reject(err) || resolve();
+    });
+  });
 };
 
 /**
@@ -23,40 +75,8 @@ module.exports = {
  */
 function getStatus() {
   return Promise.all([
-    new Promise((resolve, reject) => {
-      const connectionTest = redis.createClient({
-        host: config.cache.host,
-        port: config.cache.port,
-      });
-      connectionTest.on('error', (err) => {
-        module.exports.status.cache = false;
-        module.exports.error.cache = err;
-        connectionTest.quit();
-        connectionTest.unref();
-        reject(err);
-      }).on('connect', () => {
-        module.exports.status.cache = true;
-        module.exports.error.cache = false;
-        connectionTest.quit();
-        connectionTest.unref();
-        resolve();
-      });
-    }),
-    new Promise((resolve, reject) => {
-      const connectionTest = mysql.createConnection({
-        host: config.database.host,
-        port: config.database.port,
-        user: config.database.auth.username,
-        password: config.database.auth.password,
-        database: config.database.name,
-      });
-      connectionTest.execute('SELECT 1+1;', (err, res, fields) => {
-        module.exports.status.database = (err == null);
-        module.exports.error.database = err ? err : false;
-        connectionTest.close();
-        (err) && reject(err) || resolve();
-      });
-    }),
+    getRedisStatus(),
+    getMySqlStatus(),
   ].map(
     (promise) => promise.then(() => true).catch((err) => err)
   )).then((res) => res.reduce((prev, curr) => prev && (curr === true), true));
