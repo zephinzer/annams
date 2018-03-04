d9378f253142a068bd95e4c937f4a1da38ae216e
zephinzer <dev@joeir.net>
2018-03-04 14:02:15 +0800

    fixed code climate reusability issue

--- src/config/authn.js
+++ src/config/authn.js
@@ -1,5 +1,7 @@
 const crypto = require('crypto');
 
+const utility = require('./utility');
+
 const DEFAULT_HEALTHCHECK_USERNAME = null;
 const DEFAULT_HEALTHCHECK_PASSWORD = null;
 const DEFAULT_METRICS_USERNAME = null;
@@ -9,37 +11,35 @@ const DEFAULT_JWT_KEY_PUBLIC = null;
 
 module.exports = () => ({
   healthcheck: {
-    username: process.env.AUTHN_HEALTHCHECK_USERNAME ?
-      process.env.AUTHN_HEALTHCHECK_USERNAME : DEFAULT_HEALTHCHECK_USERNAME,
-    password: process.env.AUTHN_HEALTHCHECK_PASSWORD ?
-      process.env.AUTHN_HEALTHCHECK_PASSWORD : DEFAULT_HEALTHCHECK_PASSWORD,
+    username: utility.stringFromEnv('AUTHN_HEALTHCHECK_USERNAME', DEFAULT_HEALTHCHECK_USERNAME), // eslint-disable-line max-len
+    password: utility.stringFromEnv('AUTHN_HEALTHCHECK_PASSWORD', DEFAULT_HEALTHCHECK_PASSWORD), // eslint-disable-line max-len
   },
   metrics: {
-    username: process.env.AUTHN_METRICS_USERNAME ?
-      process.env.AUTHN_METRICS_USERNAME : DEFAULT_METRICS_USERNAME,
-    password: process.env.AUTHN_METRICS_PASSWORD ?
-      process.env.AUTHN_METRICS_PASSWORD : DEFAULT_METRICS_PASSWORD,
+    username: utility.stringFromEnv('AUTHN_METRICS_USERNAME', DEFAULT_METRICS_USERNAME), // eslint-disable-line max-len
+    password: utility.stringFromEnv('AUTHN_METRICS_PASSWORD', DEFAULT_METRICS_PASSWORD), // eslint-disable-line max-len
   },
   jwt: {
     key: {
-      private: process.env.AUTHN_JWT_KEY_PRIVATE ?
-        process.env.AUTHN_JWT_KEY_PRIVATE : DEFAULT_JWT_KEY_PRIVATE,
-      public: process.env.AUTHN_JWT_KEY_PUBLIC ?
-        process.env.AUTHN_JWT_KEY_PUBLIC : DEFAULT_JWT_KEY_PUBLIC,
+      private: utility.stringFromEnv('AUTHN_JWT_KEY_PRIVATE', DEFAULT_JWT_KEY_PRIVATE), // eslint-disable-line max-len
+      public: utility.stringFromEnv('AUTHN_JWT_KEY_PUBLIC', DEFAULT_JWT_KEY_PUBLIC), // eslint-disable-line max-len
     },
   },
 });
 
 const generateMD5Hash = (data) =>
   crypto.createHash('md5').update(data).digest('hex');
-const healthcheckPasswordFromEnvironmentHash =
-  generateMD5Hash(process.env.AUTHN_HEALTHCHECK_PASSWORD || '');
-const healthcheckPasswordHash =
-  generateMD5Hash(module.exports().healthcheck.password || '');
-const metricsPasswordFromEnvironmentHash =
-  generateMD5Hash(process.env.AUTHN_METRICS_PASSWORD || '');
-const metricsPasswordHash =
-  generateMD5Hash(module.exports().metrics.password || '');
+const healthcheckPasswordFromEnvironmentHash = generateMD5Hash(
+  process.env.AUTHN_HEALTHCHECK_PASSWORD || ''
+);
+const healthcheckPasswordHash = generateMD5Hash(
+  module.exports().healthcheck.password || ''
+);
+const metricsPasswordFromEnvironmentHash = generateMD5Hash(
+  process.env.AUTHN_METRICS_PASSWORD || ''
+);
+const metricsPasswordHash = generateMD5Hash(
+  module.exports().metrics.password || ''
+);
 console.info(`AUTHN_HEALTHCHECK_USERNAME: "${process.env.AUTHN_HEALTHCHECK_USERNAME}" (= "${module.exports().healthcheck.username}")`); // eslint-disable-line max-len
 console.info(`AUTHN_HEALTHCHECK_PASSWORD: "md5:${healthcheckPasswordFromEnvironmentHash}" (= "md5:${healthcheckPasswordHash}")`); // eslint-disable-line max-len
 console.info(`AUTHN_METRICS_USERNAME: "${process.env.AUTHN_METRICS_USERNAME}" (= "${module.exports().healthcheck.username}")`); // eslint-disable-line max-len
