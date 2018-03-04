8f5b2ee93152bf93a86c78dce5ed75cfd4171bec
zephinzer <dev@joeir.net>
2018-03-04 14:47:04 +0800

    fixed code climate verbosity and cognitive complexity issues

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const DEFAULT_ENVIRONMENT = 'development';
+const utility = require('./utility');
 
-let config = null;
+const DEFAULT_ENVIRONMENT = 'development';
 
 module.exports = initializeConfiguration;
 
@@ -15,32 +15,40 @@ module.exports = initializeConfiguration;
  * @return {Object}
  */
 function initializeConfiguration(force = false) {
-  if (config === null || force === true) {
-    config = {};
-    const configurationPath = path.join(__dirname);
+  if (initializeConfiguration.config === null || force === true) {
     console.info('> Initializing configuration (format: ENVVAR_NAME: "(provided value)" (= "[evaulated value]")'); // eslint-disable-line max-len
-    const configurationListing = fs.readdirSync(configurationPath);
-    for (let i = 0; i < configurationListing.length; ++i) {
-      if (
-        (configurationListing[i].indexOf('index.js') !== 0)
-        && (!fs.lstatSync(
-          path.join(configurationPath, configurationListing[i])
-        ).isDirectory())
-      ) {
-        const configKey =
-          configurationListing[i].substr(
-            0, configurationListing[i].lastIndexOf('.')
-          );
-        const configPath = path.join(__dirname, `/${configurationListing[i]}`);
-        const configValue = require(configPath)();
-        config[configKey] = configValue;
-      }
-    }
-    config.environment = process.env.NODE_ENV || DEFAULT_ENVIRONMENT;
-    console.info(`NODE_ENV: "${process.env.NODE_ENV}" (="${config.environment}")`); // eslint-disable-line max-len
-    config.reset = initializeConfiguration;
-    config.__timestamp = (new Date()).getTime();
+    initializeConfiguration.config = initializeConfiguration.loadConfiguration(); // eslint-disable-line max-len
+    initializeConfiguration.config.environment = utility.stringFromEnv('NODE_ENV', DEFAULT_ENVIRONMENT); // eslint-disable-line max-len
+    console.info(`NODE_ENV: "${process.env.NODE_ENV}" (="${initializeConfiguration.config.environment}")`); // eslint-disable-line max-len
+    initializeConfiguration.config.reset = initializeConfiguration.bind(null, true); // eslint-disable-line max-len
+    initializeConfiguration.config.__timestamp = (new Date()).getTime();
     console.info('/ Configuration initialized\n');
+  }
+  return initializeConfiguration.config;
+};
+
+initializeConfiguration.config = null;
+initializeConfiguration.loadConfiguration = loadConfiguration;
+
+/**
+ * @return {Object} storing the configuration
+ */
+function loadConfiguration() {
+  let config = {};
+  const configurationListing = fs.readdirSync(__dirname);
+  for (let i = 0; i < configurationListing.length; ++i) {
+    const configurationListingPath =
+      path.join(__dirname, configurationListing[i]);
+    if (
+      (configurationListing[i].indexOf('index.js') !== 0)
+      && (!fs.lstatSync(configurationListingPath).isDirectory())
+    ) {
+      const configKey = configurationListing[i].substr(
+        0, configurationListing[i].lastIndexOf('.')
+      );
+      const configValue = require(configurationListingPath)();
+      config[configKey] = configValue;
+    }
   }
   return config;
 };
