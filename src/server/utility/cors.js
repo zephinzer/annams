ac51bee75a4cb50d503840e5b7b319413d2a0b7d
zephinzer <dev@joeir.net>
2018-03-04 14:36:54 +0800

    fixed code climate cognitive complexity issue

--- src/server/utility/cors.js
+++ src/server/utility/cors.js
@@ -1,23 +1,37 @@
 const cors = require('cors');
-const config = require('../../config')();
 
-let corsHandler = null;
+module.exports = corsMiddleware;
 
-module.exports = () => {
-  if (corsHandler === null) {
-    if (config.server.cors.allowed.hosts.length === 0) {
-      corsHandler = cors();
-    } else {
-      corsHandler = cors({
-        origin: (origin, callback) => {
-          const {hosts} = config.server.cors.allowed;
-          if (hosts.indexOf(origin) !== -1 || origin === undefined) {
-            return callback(null, true);
-          }
-          return callback(new Error(`Origin ${origin} is not allowed.`));
-        },
-      });
-    }
+/**
+ * @return {Function}
+ */
+function corsMiddleware() {
+  if (corsMiddleware.corsHandler === null) {
+    corsMiddleware.initialize();
   }
-  return corsHandler;
+  return corsMiddleware.corsHandler;
+};
+
+corsMiddleware.corsHandler = null;
+corsMiddleware.initialize = initialize;
+
+/**
+ * Initializes the CORS middleware
+ */
+function initialize() {
+  const config = require('../../config')();
+  const allowedHosts = config.server.cors.allowed.hosts;
+  const corsOptions = (allowedHosts.length > 0) ? {
+    origin: (origin, callback) => {
+      if (
+        (allowedHosts.indexOf(origin) !== -1)
+        || (origin === undefined)
+      ) {
+        return callback(null, true);
+      }
+      return callback(new Error(`Origin ${origin} is not allowed.`));
+    },
+  } : undefined;
+  corsMiddleware.corsHandler =
+    cors(corsOptions);
 };
