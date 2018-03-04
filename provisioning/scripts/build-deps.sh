b9eae42d71ceaf519f64975daa25b8039bf801df
zephinzer <dev@joeir.net>
2018-03-04 12:59:28 +0800

    added test stages with docker-compose

--- provisioning/scripts/build-deps.sh
+++ provisioning/scripts/build-deps.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 DEPENDENCIES_HASH="${1}";
-ENVIRONMENT="${2}";
+if [ "${ENV}" = "" ]; then
+  ENV='production';
+fi;
 if [ "${DEPENDENCIES_HASH}" = "" ]; then
   printf -- '\e[31m\e[1m';
   printf -- 'Invalid argument provided - enter a hash as the argument to this command to use that as the dependency hash\n';
@@ -8,14 +10,9 @@ if [ "${DEPENDENCIES_HASH}" = "" ]; then
   printf -- '\e[0m';
   exit 1;
 fi;
-if [ "${ENVIRONMENT}" != "" ]; then
-  BUILD_ARGUMENTS="--build-arg NODE_ENV=${ENVIRONMENT} --build-arg YARN_FLAGS=--${ENVIRONMENT}";
-else
-  ENVIRONMENT='production';
-  BUILD_ARGUMENTS='';
-fi;
 docker build \
-  ${BUILD_ARGUMENTS} \
+  --build-arg NODE_ENV=${ENV} \
+  --build-arg YARN_FLAGS=--${ENV} \
   --file ./provisioning/images/dependencies.Dockerfile \
-  --tag zephinzer/annams:dependency-set-${ENVIRONMENT}-${DEPENDENCIES_HASH} \
+  --tag zephinzer/annams:dependency-set-${ENV}-${DEPENDENCIES_HASH} \
   .;
