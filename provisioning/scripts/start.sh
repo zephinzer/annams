382339517dd55fe0bac7134b6f525d4391acf266
zephinzer <dev@joeir.net>
2018-03-11 15:07:35 +0800

    added configuration file for nodemon to avoid watching database data locally

--- provisioning/scripts/start.sh
+++ provisioning/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-if [ "${NODE_ENV}" != "production" ]; then RUN="nodemon"; else RUN="node"; fi;
+if [ "${NODE_ENV}" != "production" ]; then RUN="nodemon --config ./.nodemon.json"; else RUN="node"; fi;
 ${RUN} ./src/entrypoint.js;
