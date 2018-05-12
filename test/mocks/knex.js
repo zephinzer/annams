d2d8270b0e08083e8698dd52f9dbc12615ba1bf9
zephinzer <dev@joeir.net>
2018-05-12 18:03:47 +0800

    refactored main knex mock

--- test/mocks/knex.js
+++ test/mocks/knex.js
@@ -4,41 +4,102 @@ const KnexMockFn = require('./knex.fn');
 const KnexMockSchema = require('./knex.schema');
 const KnexMockTable = require('./knex.table');
 
-const knex = {};
+module.exports = KnexMock;
 
-knex._ = {};
+/**
+ * Returns a mock knex instance
+ */
+function KnexMock(...args) {
+  this.databaseName = args[0];
+  this.spy = {
+    constructor: sinon.spy(),
+    limit: sinon.spy(),
+    offset: sinon.spy(),
+    orderBy: sinon.spy(),
+    select: sinon.spy(),
+    then: sinon.spy(),
+    where: sinon.spy(),
+    reset: () => {
+      Object.keys(this.spy).forEach((key) => {
+        if (typeof this.spy[key].resetHistory === 'function') {
+          this.spy[key].resetHistory();
+        }
+      });
+    },
+  };
+  this.spy.constructor.apply(this, [...args]);
 
-knex._.fn = new KnexMockFn(knex);
-knex._.schema = new KnexMockSchema(knex);
-knex._.table = new KnexMockTable(knex);
+  this.limit = function(...args) {
+    this.spy.limit.apply(this, [...args]);
+    return this;
+  };
 
-knex._.resetAll = () => {
-  knex._.spy.reset();
-  knex._.schema.spy.reset();
-  knex._.table.spy.reset();
-  knex._.fn.spy.reset();
+  this.offset = function(...args) {
+    this.spy.offset.apply(this, [...args]);
+    return this;
+  };
+
+  this.orderBy = function(...args) {
+    this.spy.orderBy.apply(this, [...args]);
+    return this;
+  };
+
+  this.select = function(...args) {
+    this.spy.select.apply(this, [...args]);
+    return this;
+  };
+
+  this.where = function(...args) {
+    this.spy.where.apply(this, [...args]);
+    return this;
+  };
+
+  this.then = function(...args) {
+    this.spy.then.apply(this, [...args]);
+    setTimeout(() => {
+      try {
+        args[0]('_then_resolved');
+      } catch (ex) {
+        console.error(ex);
+        throw ex;
+      }
+    }, 5);
+    return this;
+  };
+  return this;
 };
 
-knex._.spy = {
+KnexMock._ = {};
+
+KnexMock._.fn = new KnexMockFn(KnexMock);
+KnexMock._.schema = new KnexMockSchema(KnexMock);
+KnexMock._.table = new KnexMockTable(KnexMock);
+
+KnexMock._.resetAll = () => {
+  KnexMock._.spy.reset();
+  KnexMock._.schema.spy.reset();
+  KnexMock._.table.spy.reset();
+  KnexMock._.fn.spy.reset();
+};
+
+KnexMock._.spy = {
   raw: sinon.spy(),
   reset: () => {
-    Object.keys(knex._.spy).forEach((key) => {
-      if (typeof knex._.spy[key].resetHistory === 'function') {
-        knex._.spy[key].resetHistory();
+    Object.keys(KnexMock._.spy).forEach((key) => {
+      if (typeof KnexMock._.spy[key].resetHistory === 'function') {
+        KnexMock._.spy[key].resetHistory();
       }
     });
   },
 };
 
-knex._.mock = {
+KnexMock._.mock = {
   raw: (...args) => {
-    knex._.spy.raw.apply(null, [...args]);
+    KnexMock._.spy.raw.apply(null, [...args]);
   },
 };
 
-knex.fn = knex._.fn.mock;
-knex.schema = knex._.schema.mock;
-knex.table = knex._.table.mock;
-knex.raw = knex._.mock.raw;
-
-module.exports = knex;
+KnexMock.fn = KnexMock._.fn.mock;
+KnexMock.schema = KnexMock._.schema.mock;
+KnexMock.table = KnexMock._.table.mock;
+KnexMock.raw = KnexMock._.mock.raw;
