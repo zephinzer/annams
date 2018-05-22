cb68ff50c5f62c7eec677a0463b307cde6c33062
zephinzer <dev@joeir.net>
2018-05-22 18:23:24 +0800

    refactored test tooling

--- provisioning/scripts/test-deployment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker-compose -f ./provisioning/deployments/docker/development/docker-compose.yml config;
-docker-compose -f ./provisioning/deployments/docker/test/docker-compose.yml config;
