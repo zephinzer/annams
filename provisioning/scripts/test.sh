50266ed7940284543db0fd63f345205f63a9e792
zephinzer <dev@joeir.net>
2018-05-22 18:23:24 +0800

    refactored test tooling

--- provisioning/scripts/test.sh
+++ provisioning/scripts/test.sh
@@ -1,24 +1,47 @@
 #!/bin/sh
-printf -- "Called with arguments: $@ (->MOCHA_FLAGS)\n";
-MOCHA_FLAGS="$@";
-FLAG_WATCH='--watch';
-CALLED_WITH_COMMAND="$0 $@";
-if [ -z "${CALLED_WITH_COMMAND##*$FLAG_WATCH*}" ]; then
-  NYC_FLAGS='--silent';
-else
-  NYC_FLAGS='';
-fi;
+CHOSEN_ACTION="$1";
+
 export NODE_ENV='test';
-nyc \
-  ${NYC_FLAGS} \
-  --clean \
-  --produce-source-map \
-  --report-dir=coverage \
-  --reporter=lcov \
-  --reporter=text-lcov \
-  --show-process-tree \
-  --temp-directory=coverage/.nyc_output \
-  mocha $@ --recursive ./src/test.entrypoint.js \
-  "./db/**/*.test.js" \
-  "./src/**/*.test.js";
-nyc report --temp-directory=coverage/.nyc_output;
+
+case $CHOSEN_ACTION in
+  deployment)
+    docker-compose -f ./provisioning/deployments/docker/development/docker-compose.yml config;
+    docker-compose -f ./provisioning/deployments/docker/test/docker-compose.yml config;
+  ;;
+  lint)
+    eslint .;
+  ;;
+  sec)
+    nsp check .;
+  ;;
+  watch)
+    nyc \
+      --silent \
+      --clean \
+      --produce-source-map \
+      --report-dir=coverage \
+      --reporter=lcov \
+      --reporter=text-lcov \
+      --show-process-tree \
+      --temp-directory=coverage/.nyc_output \
+      mocha --watch --recursive ./src/test.entrypoint.js \
+      "./db/**/*.test.js" \
+      "./src/**/*.test.js";
+    nyc report --temp-directory=coverage/.nyc_output;
+  ;;
+  *)
+    nyc \
+      --silent \
+      --clean \
+      --produce-source-map \
+      --report-dir=coverage \
+      --reporter=lcov \
+      --reporter=text-lcov \
+      --show-process-tree \
+      --temp-directory=coverage/.nyc_output \
+      mocha --recursive ./src/test.entrypoint.js \
+      "./db/**/*.test.js" \
+      "./src/**/*.test.js";
+    nyc report --temp-directory=coverage/.nyc_output;
+  ;;
+esac;
