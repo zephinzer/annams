95e56b69617e02af24b832c76e74886b57db42cf
zephinzer <dev@joeir.net>
2018-05-29 17:28:05 +0800

    added password hashing to seeds

--- db/seeds/development/accounts.js
+++ db/seeds/development/accounts.js
@@ -1,21 +1,24 @@
 const faker = require('faker');
 const uuid = require('uuid/v4');
+const accountPasswordUtility = require('../../../src/account/utility/password');
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
+  // Deletes ALL existing entries before creating new ones
+  // Seed user passwords are their emails, we use the generated UUID
+  // for the salting
   return knex('accounts').del()
     .then(() => {
       const userCount = 50;
       let mockUsersSeeds = [];
       for (let id = 1; id < userCount; ++id) {
+        const userUuid = uuid();
+        const userEmail = faker.internet.email();
         mockUsersSeeds.push({
           id,
-          uuid: uuid(),
-          email: faker.internet.email(),
+          uuid: userUuid,
+          email: userEmail,
           username: faker.internet.userName(),
-          password: faker.internet.password(24),
-          created_at: null,
-          updated_at: null,
+          password: accountPasswordUtility.hash(userEmail, userUuid),
         });
       }
       return knex('accounts').insert(mockUsersSeeds);
