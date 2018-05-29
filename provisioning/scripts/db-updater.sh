8ded57981fd39a7a890125f7ab334caa3e42c278
zephinzer <dev@joeir.net>
2018-05-29 17:27:43 +0800

    made script more verbose so its more understanable

--- provisioning/scripts/db-updater.sh
+++ provisioning/scripts/db-updater.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+printf -- 'From environment:\n';
 if [ "${DB_HOST}" = "" ]; then DB_HOST=dev_database; fi;
+printf -- "  DB_HOST:          ${DB_HOST}\n";
 if [ "${DB_PORT}" = "" ]; then DB_PORT=3306; fi;
+printf -- "  DB_PORT:          ${DB_PORT}\n";
+if [ "${ENABLE_MIGRATION}" = "" ]; then ENABLE_MIGRATION=0; fi;
+printf -- "  ENABLE_MIGRATION: ${ENABLE_MIGRATION}\n";
+if [ "${ENABLE_SEEDING}" = "" ]; then ENABLE_SEEDING=0; fi;
+printf -- "  ENABLE_SEEDING:   ${ENABLE_SEEDING}\n";
+printf -- '-\n';
 
 DATABASE_UP=0;
 printf -- "Waiting for MySQL at '${DB_HOST}:${DB_PORT}' to be up..";
