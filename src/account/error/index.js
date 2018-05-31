c4a25b360ae78aa0071c0f13b9766bfd608e30a2
zephinzer <dev@joeir.net>
2018-06-01 00:00:57 +0800

    fixed lint issues

--- src/account/error/index.js
+++ src/account/error/index.js
@@ -2,25 +2,25 @@ module.exports = {
   accountNotFound: (uuid) => {
     const code = 'ERROR_ACCOUNT_NOT_FOUND';
     const status = 404;
-    const error = new Error(`The account with UUID ("${uuid}") could not be found.`);
+    const error = new Error(`The account with UUID ("${uuid}") could not be found.`); // eslint-disable-line max-len
     return Object.assign(error, {code, status});
   },
   invalidField: (fieldName, userValue) => {
     const code = 'ERROR_ACCOUNT_FIELD_INVALID';
     const status = 400;
-    const error = new Error(`The provided ${fieldName} ("${userValue}") is invalid.`);
+    const error = new Error(`The provided ${fieldName} ("${userValue}") is invalid.`); // eslint-disable-line max-len
     return Object.assign(error, {code, status});
   },
   invalidSecureField: (fieldName) => {
     const code = 'ERROR_ACCOUNT_FIELD_INVALID';
     const status = 400;
-    const error = new Error(`The provided ${fieldName} (-REDACTED-) is invalid.`);
+    const error = new Error(`The provided ${fieldName} (-REDACTED-) is invalid.`); // eslint-disable-line max-len
     return Object.assign(error, {code, status});
   },
   missingField: (fieldName) => {
     const code = 'ERROR_ACCOUNT_FIELD_NOT_FOUND';
     const status = 400;
-    const error = new Error(`The required field ${fieldName} was not found.`);
+    const error = new Error(`The required field ${fieldName} was not found.`); // eslint-disable-line max-len
     return Object.assign(error, {code, status});
   },
 };
